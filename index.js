db5c0a289b32e93268eca90f258666b518fbf774
ngr.r <91729773+ngrnr@users.noreply.github.com>
2022-06-04 08:17:59 +0700

    Nugraha Ramadan - commited

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
-
 // TODO: Fungsi utama
 // Menerima 2 parameter: list nama dan callback sort
 // Mengembalikan array of string dengan format:
@@ -7,24 +6,41 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "2. Nama"
 // ...
 const sorter = null;
+const sorter = (names,sort) => {
+  let fun = sort(names);
+  nameOfString = [];
+  for (let counter = 0; counter < fun.length; counter++) {
+    nameOfString.push((counter+1) + ". " + fun[counter]);
+  }
+  return nameOfString;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
 const sortAscending = null;
+const sortAscending= (names) => {
+  return names.sort();
+};
+console.log(sortAscending(names));
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
 const sortDescending = null;
 
+const sortDescending = (names) => {
+  return names.sort().reverse();
+};
+console.log(sortDescending(names));
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
   console.log(sorter?.(names, sortDescending)?.join("\n"));
 })();
-
 module.exports = {
   sorter,
   sortAscending,
   sortDescending,
   names,
 };
+
+
